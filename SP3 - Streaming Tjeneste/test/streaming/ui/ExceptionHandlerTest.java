592b03e52f06ed86cc83b944c705e6c0db983a18
Odiegenx <124783324+Odiegenx@users.noreply.github.com>
2023-04-27 12:33:23 +0200

    updated stuff

--- SP3 - Streaming Tjeneste/test/streaming/ui/ExceptionHandlerTest.java
+++ SP3 - Streaming Tjeneste/test/streaming/ui/ExceptionHandlerTest.java
@@ -1,11 +1,9 @@
 package streaming.ui;
 
 import streaming.exceptions.InValidPasswordException;
-import streaming.exceptions.InValidUsername;
+import streaming.exceptions.InValidUsernameException;
 
 import java.io.FileNotFoundException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ExceptionHandlerTest {
 
@@ -13,7 +11,7 @@ class ExceptionHandlerTest {
     void catchException() {
         ExceptionHandler ged = new ExceptionHandler();
         Exception ie = new InValidPasswordException("Password wrong");
-        Exception ie2 = new InValidUsername("Username issue");
+        Exception ie2 = new InValidUsernameException("Username issue");
         Exception ie3 = new InValidPasswordException("Password wrong");
         Exception ie4 = new FileNotFoundException();
         ged.catchException(ie);
