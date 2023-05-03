c36b98639b65eff89f1339e2bce9759e3db38c09
Sir Rolin <nicolaieno@msn.com>
2023-05-03 13:35:33 +0200

    Media Search på DB

--- SP3 - Streaming Tjeneste/src/Streaming/io/db/DBIO.java
+++ SP3 - Streaming Tjeneste/src/Streaming/io/db/DBIO.java
@@ -1,28 +1,77 @@
 package streaming.io.db;
 
 import streaming.io.IO;
-import streaming.mediaHandler.Media;
 import streaming.users.User;
 
+import streaming.mediaHandler.Media;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class DBIO implements IO {
-    @Override
-    public ArrayList<String> readDataMedia() throws FileNotFoundException {
+    private ArrayList<Character> getDistinct(String input){
+        ArrayList<Character> output = new ArrayList<>();
+        for(int i = 0; i < input.length();++i){
+            if(!output.contains(input.charAt(i))){
+                output.add(input.charAt(i));
+            }
+        }
+        return output;
+    }
+    public ArrayList<String> searchMedia(String name, String genre, String year, float min, float max) {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
         conn = DBConnector.getConn();
         try {
-            stmt = conn.prepareStatement("select * from streaming.film");
-            rs = stmt.executeQuery("select * from streaming.film");
+            int likenessAtLeast = 0;
+            String table = "select name, year, genre, rating, null as seasons from film union select name, year, genre, rating, seasons from serier";
+            String sql = "select * from ( select name, year, genre, rating, seasons";
+            if(name != null){
+                sql += ", (case when name like \"%" + name + "%\" then 100 else 0 end) + " +
+                    "(length(name) - length(";
+                ArrayList<Character> chars = getDistinct(name);
+                likenessAtLeast += chars.size();
+                for(char c: chars){
+                    sql += "replace(";
+                }
+                sql += "lower(name)";
+                for(char c: chars){
+                    sql += ", \"" + c + "\",\"\")";
+                }
+                sql += ")) as likeness";
+            }
+            sql += " from ("+ table + ") as tbinner) as tbouter where 1=1";
+            if(name != null){
+                sql += " and likeness > " + Integer.toString(likenessAtLeast);
+            }
+            if(year != null){
+                sql += " and year like \"%" + year + "%\"";
+            }
+            if(genre != null){
+                sql += " and genre like \"%" + genre + "%\"";
+            }
+            if(min != 0){
+                sql += " and rating > " + min;
+            }
+            if(max != 0){
+                sql += " and rating < " + max;
+            }
+            if(name != null){
+                sql += " order by likeness desc, length(name)";
+            }
+            stmt = conn.prepareStatement(sql);
+            System.out.println(sql);
+            rs = stmt.executeQuery(sql);
 
         } catch (SQLException e){
             e.printStackTrace();
         }
+        return getMediaStringsFromResult(conn, stmt, rs);
+    }
+
+    private ArrayList<String> getMediaStringsFromResult(Connection conn, PreparedStatement stmt, ResultSet rs) {
         ArrayList<String> output = new ArrayList<>();
         try {
             while(rs.next()){
@@ -46,6 +95,23 @@ public class DBIO implements IO {
             }//end finally try
         }//end try
         return output;
+    }
+
+    @Override
+    public ArrayList<String> readDataMedia() throws FileNotFoundException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        conn = DBConnector.getConn();
+        try {
+            String sql = "select * from streaming.film";
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery(sql);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return getMediaStringsFromResult(conn, stmt, rs);
     }
 
     @Override
@@ -137,6 +203,16 @@ public class DBIO implements IO {
         }//end try
         return output;
     }
+    private String getStringFromMediaList(ArrayList<Media> media){
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < media.size();i++){
+            output.append(media.get(i).getName());
+            if(i < media.size()-1){
+                output.append(",");
+            }
+        }
+        return output.toString();
+    }
 
     @Override
     public void writeDataUser(User currentUser) {
@@ -150,7 +226,7 @@ public class DBIO implements IO {
             rs = stmt.executeQuery(sql);
             if(rs.next() && rs.getInt("count(*)") > 0) {
                 rs.close();
-                sql = "update streaming.users set saved = \"" + currentUser.getSavedMedia() + "\", watched = \"" + currentUser.getWatchedMedia() + "\"";
+                sql = "update streaming.users set saved = \"" + getStringFromMediaList(currentUser.getSavedMedia()) + "\", watched = \"" + getStringFromMediaList(currentUser.getWatchedMedia()) + "\" where ID = " + currentUser.getId();
                 stmt = conn.prepareStatement(sql);
                 stmt.executeUpdate(sql);
             } else {
